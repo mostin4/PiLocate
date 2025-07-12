6ed131a2e4bf196b248b4a0d2ce3a339f807476c
mostin4 <tayfunus@hotmail.com>
2025-07-12 23:26:08 +0300

    MapView.tsx

--- frontend/components/MapView.tsx
+++ frontend/components/MapView.tsx
@@ -1,9 +1,25 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import BusinessCard from './BusinessCard';
 
-export default function MapView({ kybList }: { kybList: any[] }) {
+interface Business {
+  name: string;
+  lat: number;
+  lng: number;
+  description: string;
+  isPiAccepted: boolean;
+  daoScore: number;
+  contactLink: string;
+}
+
+export default function MapView({ kybList }: { kybList: Business[] }) {
   return (
-    <MapContainer center={[41.0082, 28.9784]} zoom={13} scrollWheelZoom={true} style={{ height: '80vh', width: '100%' }}>
+    <MapContainer
+      center={[41.0082, 28.9784]}
+      zoom={13}
+      scrollWheelZoom={true}
+      style={{ height: '80vh', width: '100%' }}
+    >
       <TileLayer
         attribution='&copy; OpenStreetMap katkıcıları'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -11,8 +27,7 @@ export default function MapView({ kybList }: { kybList: any[] }) {
       {kybList.map((business, index) => (
         <Marker key={index} position={[business.lat, business.lng]}>
           <Popup>
-            <strong>{business.name}</strong><br />
-            {business.description}
+            <BusinessCard data={business} />
           </Popup>
         </Marker>
       ))}
